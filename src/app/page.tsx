a9f3832b89bdfab3b680f27b5526c8ae7a8580c1
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2024-09-22 20:45:30 +0200

    feat: remove unused page chore: upgrade packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffectOnce } from "@/hooks/useEffectOnce";
-import { convertFileSrc, invoke } from "@tauri-apps/api/core";
 import { open } from '@tauri-apps/plugin-dialog';
 import { readFile } from "@tauri-apps/plugin-fs";
 import { useState } from "react";
@@ -10,7 +9,6 @@ export default function Home() {
   const [filePath, setFilePath] = useState<string | null>(null)
 
   useEffectOnce( ()=>{
-    invoke('set_complete', {task: 'frontend'})
     
     open({
       multiple: false,
@@ -30,7 +28,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     {filePath}
-    <img src={filePath??""} className="w-96 h-96" />
+    <img src={filePath??""} className="w-96 h-96 object-cover" />
      </div>
   );
 }
--- src/app/splashscreen/page.tsx
+++
@@ -1,5 +0,0 @@
-function SplashScreenPage() {
-  return <div style={{width:100,height:100,background:"red"}}></div>;
-}
-
-export default SplashScreenPage;
